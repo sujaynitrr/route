3e2baa2897c3d843a819b7f372c12ca6e9c508a4
SujayKumar <sujay.nitrr@gmail.com>
2023-08-30 16:05:59 +0530

    route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,46 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ResolveFn, RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './Component/first/first.component';
+import { SecondComponent } from './Component/second/second.component';
+import { PageNotFoundComponent } from './Component/page-not-found/page-not-found.component';
+import { ChildAComponent } from './Component/child-a/child-a.component';
+import { ChildBComponent } from './Component/child-b/child-b.component';
+const resolvedChildATitle: ResolveFn<string> = () => Promise.resolve('child a');
+console.log(resolvedChildATitle, 'resolvedChildATitle');
+const routes: Routes = [
+  {
+    path: 'first-component',
+    component: FirstComponent,
+    children: [
+      {
+        path: 'child-a',
+        title: 'child-a',
+        component: ChildAComponent,
+      },
+      {
+        path: 'child-b',
+        title: 'child-b',
+        component: ChildBComponent,
+      },
+    ],
+  },
+  {
+    path: 'second-component',
+    component: SecondComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'first-component',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './Component/first/first.component';
+import { SecondComponent } from './Component/second/second.component';
+import { PageNotFoundComponent } from './Component/page-not-found/page-not-found.component';
+import { ChildAComponent } from './Component/child-a/child-a.component';
+import { ChildBComponent } from './Component/child-b/child-b.component';
+import { CrisisListComponent } from './Component/crisis-list/crisis-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    PageNotFoundComponent,
+    ChildAComponent,
+    ChildBComponent,
+    CrisisListComponent
   ],
   imports: [
     BrowserModule,
